c7147bdd6144924429fe690de0328b489d5faa6f
Yashvi Desaii <yashvi.desai999@gmail.com>
2022-11-18 14:04:41 -0800

    -added files to set up routing

--- airportmgr/frontend/src/components/App.js
+++ airportmgr/frontend/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-//import HomePage from "./HomePage";
+import HomePage from "./HomePage";
+import AirlineEmployeePage from "./AirlineEmployeePage";
+import AirportEmployeePage from "./AirportEmployeePage";
 
 export default class App extends Component {
   constructor(props) {
@@ -8,7 +10,13 @@ export default class App extends Component {
   }
 
   render() {
-    return <h1>Testing React Code</h1>
+    return (
+    <div>
+      <HomePage />
+      <AirlineEmployeePage />
+      <AirportEmployeePage />
+    </div>
+    );
   }
 }
 
