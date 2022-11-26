956bc9dcadb7c58da59cacf5407adb729780f94d
Pratik Mali <38047130+impratik7@users.noreply.github.com>
2022-11-26 00:43:31 -0800

    Created CreateAirline api (/create-airline)

--- airportmgr/api/urls.py
+++ airportmgr/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import FlightView, AirlineView, EmployeeView, GateView, BaggageView, CreateFlightView
+from .views import FlightView, AirlineView, EmployeeView, GateView, BaggageView, CreateFlightView, CreateAirlineView
 
 urlpatterns = [
     path('flight', FlightView.as_view()),
     path('create-flight', CreateFlightView.as_view()),
     path('airline', AirlineView.as_view()),
+    path('create-airline', CreateAirlineView.as_view()),
     path('employee', EmployeeView.as_view()),
     path('gate', GateView.as_view()),
     path('baggage', BaggageView.as_view()),
