c400eea58536418fe9f0ad384191c806b4cc9af1
Yashvi Desaii <yashvi.desai999@gmail.com>
2022-12-04 17:43:55 -0800

    managed form routing

--- airportmgr/frontend/src/components/FlightUpdateForm.js
+++ airportmgr/frontend/src/components/FlightUpdateForm.js
@@ -65,7 +65,7 @@ const FlightUpdateForm = () => {
     };
     
     const handleSubmit = e => {
-        e.preventDefault();
+        //e.preventDefault();
 
         console.log(flightCode,flightSource, flightDestination, value, flightStatusDropdownValue, flightTypeDropdownValue);
         //handleClose();
