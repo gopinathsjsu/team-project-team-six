f4a162607b769dc56964d360afe6c973378c2765
unknown <sayalisn4@gmail.com>
2022-12-04 21:24:29 -0800

    Comments and clean up app file

--- airportmgr/frontend/src/components/App.js
+++ airportmgr/frontend/src/components/App.js
@@ -1,13 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
-import AirlineEmployeePage from "./AirlineEmployeePage";
-import AirportEmployeePage from "./AirportEmployeePage";
 import "./App.css";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
 import './FlightForm.css'
 
 export default class App extends Component {
